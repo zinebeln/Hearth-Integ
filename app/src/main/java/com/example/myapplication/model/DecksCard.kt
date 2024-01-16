95729dfe38a5b03cdc2ea4a25dc6a5cdb469b374
Zineb <zineb.elkh@yahoo.com>
2024-01-16 09:58:10 +0100

    design des pages

--- app/src/main/java/com/example/myapplication/model/DecksCard.kt
+++ app/src/main/java/com/example/myapplication/model/DecksCard.kt
@@ -1,7 +1,21 @@
 package com.example.myapplication.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+
+
+
+//
+
+//    foreignKeys = [
+//    ForeignKey(
+//        entity = User::class,
+//        parentColumns = ["id"],
+//        childColumns = ["userId"],
+//        onDelete = ForeignKey.CASCADE
+//    )
+//])
 
 @Entity(tableName = "decks_cards")
 data class DecksCard(
