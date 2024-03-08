81a5c4c0966205243fac6ff0c263a3a4ed0b0725
Zineb <zineb.elkh@yahoo.com>
2024-03-08 09:52:35 +0100

    clean code

--- app/src/main/java/com/example/myapplication/model/Infos.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.model
-
-data class Infos (
-    var patch: String,
-    var classes: List<String>
-    )
--- app/src/main/java/dataBase/CardsDBDataSource.kt
+++
@@ -1,6 +0,0 @@
-package dataBase
-
-object CardsDBDataSource {
-
-
-}
